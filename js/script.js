7c87b0684aa1e2337d5702104e60ec3d2a9f6b63
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-12 20:10:30 +0300

    Initailized variables in JS

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Initialize variables
+let username = document.getElementById("fullname");
+let phoneNumber = document.getElementById("phone_number");
+let email = document.getElementById("email")
+let message = document.getElementById("message")
