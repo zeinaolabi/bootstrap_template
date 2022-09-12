675e369308dfbe6356d75ee1830bbc04bc2d5a07
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-12 20:27:48 +0300

    Added validateName function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 //Initialize variables
-let username = document.getElementById("fullname");
-let phoneNumber = document.getElementById("phone_number");
-let email = document.getElementById("email")
-let message = document.getElementById("message")
+let username = document.getElementById("fullname").value;
+let phoneNumber = document.getElementById("phone_number").value;
+let email = document.getElementById("email").value;
+let message = document.getElementById("message").value;
+let error = document.getElementById("error").value;
+
+function validateName(){
+    if(typeof username !== "string" || /[^A-Za-z]+/g.test(username) || username == ""){
+        error.textContent="Error: Invalid username"
+    }
+    else if(username.trim().length() < 5){
+        error.textContent="Error: Name should have a minimum of 5 characters"
+    }
+}
