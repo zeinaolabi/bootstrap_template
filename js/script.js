68971f165d3b045a1976f718fac29841fc042278
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-12 20:54:09 +0300

    Added validatePhoneNumber function

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let message = document.getElementById("message").value;
 let error = document.getElementById("error").value;
 let submitButton = document.getElementById("submit");
 
-function validateName(){
+function validateName(username, error){
     if(typeof username !== "string" || /[^A-Za-z]+/g.test(username) || username == ""){
         error.textContent="Error: Invalid username"
     }
@@ -15,11 +15,26 @@ function validateName(){
     }
 }
 
-function validateEmail(){
+function validateEmail(email, error){
     if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) || 
     email.substring(0,email.indexOf("@")).length < 3 ||
     email.substring(email.indexOf("@") + 1,email.length).length < 5
     ){
         error.textContent="Error: Invalid email";
+    }
+}
+
+function validatePhoneNumber(phoneNumber, error){
+    if(phoneNumber.substring(0,3).length != "+961"){
+        error.textContent="Error: Invalid country code"
+    }
+    else if(phoneNumber.substring(4, phoneNumber.length).length > 8 || 
+    phoneNumber.substring(4, phoneNumber.length).length < 7){
+        error.textContent="Error: Phone number should be 7 or 8 digits"
+    }
+    else if((phoneNumber.substring(4, phoneNumber.length) == 7 && phoneNumber[4] != "3") ||
+    (phoneNumber.substring(4, phoneNumber.length) == 8 && (phoneNumber.substring(4,6) != "71" || phoneNumber.substring(4,6) != "76" || phoneNumber.substring(4,6) != "70")
+    )){
+        error.textContent="Error: Invalid phone number"
     }
 }
