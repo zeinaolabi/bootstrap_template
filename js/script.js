b2783c526c42916b028aff9986b3226bd7cb8a46
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-12 20:35:20 +0300

    Added validateEmail function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let phoneNumber = document.getElementById("phone_number").value;
 let email = document.getElementById("email").value;
 let message = document.getElementById("message").value;
 let error = document.getElementById("error").value;
+let submitButton = document.getElementById("submit");
 
 function validateName(){
     if(typeof username !== "string" || /[^A-Za-z]+/g.test(username) || username == ""){
@@ -11,5 +12,14 @@ function validateName(){
     }
     else if(username.trim().length() < 5){
         error.textContent="Error: Name should have a minimum of 5 characters"
+    }
+}
+
+function validateEmail(){
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) || 
+    email.substring(0,email.indexOf("@")).length < 3 ||
+    email.substring(email.indexOf("@") + 1,email.length).length < 5
+    ){
+        error.textContent="Error: Invalid email";
     }
 }
