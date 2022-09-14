23703ed074c085476d8bf0982da846551965e394
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-14 09:03:11 +0300

    Added add message API

---
+++ backend/add_message.php
@@ -0,0 +1,18 @@
+<?php
+include("connection.php");
+require_once("headers.php");
+
+$fullName = $_POST["full_name"];
+$email = $_POST["email"];
+$phoneNumber = $_POST["phone_number"];
+$message = $_POST["message"];
+
+$query = $mysqli->prepare("INSERT INTO messages(full_name, email, phone_number, message) VALUES  (?, ?, ?, ?)");
+$query->bind_param("ssis", $fullName, $email, $phoneNumber, $message);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+?>
